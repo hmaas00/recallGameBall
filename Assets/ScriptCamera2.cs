0cd7b703a13ec78888111ca165f17db3c5950ff1
harlan maas <hmaas00@gmail.com>
2017-11-23 22:57:53 -0200

    adicao da camera

---
+++ Assets/ScriptCamera2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptCamera2 : MonoBehaviour {
+
+    public GameObject Target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(Target.transform);
+	}
+}
